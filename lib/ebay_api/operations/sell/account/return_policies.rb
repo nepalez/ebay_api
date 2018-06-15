27d0016a3c3b90b7f67805e14e51c4e87be72ae3
Milosz Jerkiewicz <milj@users.noreply.github.com>
2018-06-15 09:31:15 +0200

    Add sell/account/return_policies/update operation

--- lib/ebay_api/operations/sell/account/return_policies.rb
+++ lib/ebay_api/operations/sell/account/return_policies.rb
@@ -1,5 +1,6 @@
 require_relative "return_policies/create"
 require_relative "return_policies/delete"
+require_relative "return_policies/update"
 require_relative "return_policies/list"
 
 class EbayAPI
