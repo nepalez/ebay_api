d6dce110b31c0fda93aef3b19e64b3a8ef9d3bad
Valentine Kiselev <mrexox@evilmartians.com>
2021-12-13 17:46:42 +0300

    Save heavy responses to a file for fetch_item_aspects (#13)
    * Save heavy responses to a file for fetch_item_aspects
    
    Signed-off-by: Valentin Kiselev <mrexox@evilmartians.com>
    
    * Update lib/ebay_api/middlewares/save_to_file_response.rb
    
    Co-authored-by: Andrey Novikov <envek@envek.name>
    
    * Bump version
    
    Co-authored-by: Andrey Novikov <envek@envek.name>

--- lib/ebay_api/middlewares.rb
+++ lib/ebay_api/middlewares.rb
@@ -1,4 +1,4 @@
 require_relative "middlewares/json_response"
-require_relative "middlewares/gzipped_response"
+require_relative "middlewares/save_to_file_response"
 require_relative "middlewares/paginated_collection"
 require_relative "middlewares/log_request"
